bcd1acb992e856202a63634aeb732d8e114fa5d7
ritiek <ritiekmalhotra123@gmail.com>
2017-10-15 18:56:56 +0530

    Specify handwriting style

--- get_hand.py
+++ get_hand.py
@@ -15,7 +15,10 @@ def get_arguments():
     group.add_argument(
         '-f', '--file', help='text file to generate handwriting from')
     group.add_argument(
-        '-s', '--string', help='string to generate handwriting from')
+        '-t', '--text', help='text to generate handwriting from')
+    parser.add_argument(
+        '-s', '--style', help='handwriting style from 0 to 5, where 5 is random style',
+        type=int, default=5)
     parser.add_argument(
         '-b', '--bias', help='handwriting bias from 0.1 to 1',
         type=float, default=0.8)
@@ -29,9 +32,17 @@ def get_arguments():
     return parser.parse_args()
 
 
-def get_handwriting(text, bias=0.8, samples=1):
+def get_handwriting(text, style, bias, samples=1):
+    style_models = (
+            '../data/trainset_diff_no_start_all_labels.nc,973+905',
+            '../data/trainset_diff_no_start_all_labels.nc,1082+554',
+            '../data/trainset_diff_no_start_all_labels.nc,1495+898',
+            '../data/trainset_diff_no_start_all_labels.nc,1970+378',
+            '../data/trainset_diff_no_start_all_labels.nc,1561+872',
+            '')
+
     payload = {'text': text,
-               'style': '../data/trainset_diff_no_start_all_labels.nc,1082+554',
+               'style': style_models[style],
                'bias': bias,
                'samples': samples}
     url = 'http://www.cs.toronto.edu/~graves/handwriting.cgi'
@@ -63,9 +74,10 @@ def command_line():
     if args.file:
         with open(args.file, 'r') as fl:
             text_in = [i.strip() for i in fl.readlines()]
-    elif args.string:
-        text_in = [args.string]
+    elif args.text:
+        text_in = [args.text]
 
+    style = args.style
     start_at = args.position
     bias = args.bias
 
@@ -84,14 +96,14 @@ def command_line():
             continue
         if not line.strip() == '':
             print(index, ' - ', line, end='')
-            image_str = get_handwriting(line, bias)
+            image_str = get_handwriting(line, style, bias)
             x = base64.b64decode(image_str)
             image_out = os.path.join('images', str(index) + '.png')
             with open(image_out, 'wb') as fl:
                 fl.write(x)
             print('|')
 
-    if not color == [0,0,0]:
+    if not color == [0, 0, 0]:
         add_color(color, image_out)
 
 
